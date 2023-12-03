42b457436618b16b1b46cad3d65ead637e24e966
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-12-04 02:28:44 +0600

    updated fetch getMe logic

--- src/app/(base)/page.uc.tsx
+++ src/app/(base)/page.uc.tsx
@@ -7,28 +7,30 @@ import { errorNotification } from "@/lib/utils/notification";
 export default function HomeClient() {
   const [me, setMe] = useState<User | null>(null);
 
-  const getMe = async () => {
-    try {
-      const data: User = await API.auth.getMe();
-
-      if (data) {
-        setMe(data);
-      }
-    } catch (e: any) {
-      errorNotification("Something went wrong");
-      console.error(e);
-    }
-  };
-
   useEffect(() => {
+    const getMe = async () => {
+      try {
+        const data: User = await API.auth.getMe();
+
+        if (data) {
+          setMe(data);
+        }
+      } catch (e: any) {
+        errorNotification("Something went wrong");
+        console.error(e);
+      }
+    };
+
     getMe();
-  }, [getMe]);
+  }, []);
 
   return (
     <>
       <div>
         {me ? (
-          <p>Hi {me.firstName}, it turns out you're in {me.location[0].city} 😯</p>
+          <p>
+            Hi {me.firstName}, it turns out you're in {me.location[0].city} 😯
+          </p>
         ) : (
           <p>Loading...</p>
         )}
